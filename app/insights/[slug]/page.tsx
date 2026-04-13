5f84f731d3e116007c1079e827d3f524eb835002
jlindl <jacklindo31@icloud.com>
2026-04-13 09:34:47 +0100

    fix

--- app/insights/[slug]/page.tsx
+++ app/insights/[slug]/page.tsx
@@ -113,7 +113,7 @@ export default async function PostPage({ params }: PostPageProps) {
                             </button>
                             <button className="w-10 h-10 rounded-full border border-metallic/20 flex items-center justify-center hover:bg-signal/10 hover:border-signal/30 transition-all text-metallic hover:text-signal">
                                 <span className="sr-only">Share on LinkedIn</span>
-                                <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></button>
+                                <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></button>
                         </div>
                     </div>
                 </div>
